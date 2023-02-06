4e9d31883874b6d687ad3462567e42eba7826708
Monu Goswami <monu1goswami@gmail.com>
2023-02-06 13:54:37 +0530

    if applied, Automation script updated

--- src/test/java/Errorvalidation.java
+++ src/test/java/Errorvalidation.java
@@ -1,9 +1,38 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Errorvalidation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Launch Browser
+		WebDriver driver = new ChromeDriver();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		//Open url on the Browser
+		driver.get("https://osa-web.t-cg.co.uk/");
+		driver.manage().window().maximize();
+		
+		//Search for B16 8PE in search box
+		driver.findElement(By.id("main")).sendKeys("B16 8PE");
+			
+		//click on Search Icon
+		driver.findElement(By.id("searchPostcodeButton")).click();
+		
+		
+		//scroll to the element Contact group
+		WebElement flag = driver.findElement(By.className("text-irisBlue"));
+		js.executeAsyncScript("arguments[0].scrollIntoView();", flag);
+		
+		//driver.quit();
+		
+		
+		
+		
 	}
 
 }
